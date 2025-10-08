0be7f36f265159b37b7bec29df4b3d9dd93e4efa
BoriskaGlebov <boristheblade.glebov@yandex.ru>
2025-10-08 15:11:29 +0300

    схема добавлена

---
+++ bot/users/schemas.py
@@ -0,0 +1,34 @@
+from typing import Optional
+
+from pydantic import BaseModel, ConfigDict, Field
+
+
+class SUserTelegramID(BaseModel):
+    """Схема для идентификации пользователя по Telegram ID.
+
+    Attributes
+        telegram_id (int): Уникальный идентификатор пользователя в Telegram.
+
+    """
+
+    telegram_id: int = Field(
+        ..., description="Уникальный идентификатор пользователя в Telegram"
+    )
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class SUser(SUserTelegramID):
+    """Схема пользователя Telegram с расширенной информацией.
+
+    Attributes
+        telegram_id (int): Уникальный идентификатор пользователя в Telegram.
+        username (Optional[str]): Имя пользователя в Telegram.
+        first_name (Optional[str]): Имя пользователя.
+        last_name (Optional[str]): Фамилия пользователя.
+
+    """
+
+    username: Optional[str] = Field(None, description="Имя пользователя в Telegram")
+    first_name: Optional[str] = Field(None, description="Имя пользователя")
+    last_name: Optional[str] = Field(None, description="Фамилия пользователя")
