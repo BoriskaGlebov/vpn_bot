dae81deb6a3b2af3aead4d401a15109ee61202db
BoriskaGlebov <boristheblade.glebov@yandex.ru>
2025-10-18 16:34:01 +0300

    Проверка vpn router

--- bot/utils/start_stop_bot.py
+++ bot/utils/start_stop_bot.py
@@ -2,7 +2,8 @@ from typing import Optional
 
 from aiogram import Bot
 from aiogram.exceptions import TelegramBadRequest
-from aiogram.types import InlineKeyboardMarkup
+from aiogram.types import InlineKeyboardMarkup, Message
+from redis_manager import SettingsRedis
 
 from bot.config import bot, logger, settings_bot
 from bot.utils.commands import set_bot_commands
@@ -10,15 +11,21 @@ from bot.utils.set_description_file import set_description
 
 
 async def send_to_admins(
-    bot: Bot, message_text: str, reply_markup: Optional[InlineKeyboardMarkup] = None
+    bot: Bot,
+    message_text: str,
+    reply_markup: Optional[InlineKeyboardMarkup] = None,
+    telegram_id: Optional[int] = None,
+    redis_manager: Optional[SettingsRedis] = None,
 ) -> None:
     """Отправляет сообщение всем администраторам с возможной inline-клавиатурой.
 
     Args:
         bot (Bot): Экземпляр бота Aiogram.
+        redis_manager (SettingsRedis): хранение сообщений админа для дальнейшего изменения
         message_text (str): Текст сообщения для отправки администраторам.
         reply_markup (Optional[InlineKeyboardMarkup], optional): Inline-клавиатура для сообщения.
             По умолчанию None.
+        telegram_id ( Optional[int]): Идентификатор пользователя от которого обновления.
 
     Returns
         None
@@ -30,14 +37,51 @@ async def send_to_admins(
     """
     for admin_id in settings_bot.ADMIN_IDS:
         try:
-            await bot.send_message(
+            mes: Message = await bot.send_message(
                 chat_id=admin_id, text=message_text, reply_markup=reply_markup
             )
+            if telegram_id and redis_manager:
+                await redis_manager.save_admin_message(
+                    user_id=telegram_id, admin_id=admin_id, message_id=mes.message_id
+                )
         except TelegramBadRequest as e:
             logger.bind(user=admin_id).error(
                 f"Не удалось отправить сообщение админу {admin_id}: {e}"
             )
             continue
+
+
+async def edit_admin_messages(
+    bot: Bot,
+    user_id: int,
+    new_text: str,
+    redis_manager: SettingsRedis,
+) -> None:
+    """Редактирует все сообщения администраторов, относящиеся к конкретному пользователю.
+
+    Args:
+        bot (Bot): Экземпляр бота Aiogram.
+        user_id (int): Telegram ID пользователя, к которому относятся сообщения.
+        redis_manager (SettingsRedis): хранение сообщений админа для дальнейшего изменения
+        new_text (str): Новый текст для сообщений админов.
+
+    Returns
+        None
+
+    """
+    admin_messages = await redis_manager.get_admin_messages(user_id)
+    for msg in admin_messages:
+        try:
+            await bot.edit_message_text(
+                chat_id=msg["chat_id"], message_id=msg["message_id"], text=new_text
+            )
+        except TelegramBadRequest:
+            logger.warning(
+                f"Не удалось отредактировать сообщение {msg['chat_id']}:{msg['message_id']}"
+            )
+            continue
+
+    await redis_manager.clear_admin_messages(user_id)
 
 
 async def start_bot() -> None:
