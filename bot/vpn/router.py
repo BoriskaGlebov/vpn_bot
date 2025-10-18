3b44da8d1b19d3062524f443f17e4e8a6484dcb7
BoriskaGlebov <boristheblade.glebov@yandex.ru>
2025-10-18 16:30:47 +0300

    Проверка vpn router

--- bot/vpn/router.py
+++ bot/vpn/router.py
@@ -1,6 +1,7 @@
 from aiogram import F, Router
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message, ReplyKeyboardRemove
+from aiogram.utils.chat_action import ChatActionSender
 from config import bot
 from database import connection
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -19,17 +20,21 @@ vpn_router = Router()
 @vpn_router.message(F.text.contains("🔑 Получить VPN-конфиг AmneziaVPN"))  # type: ignore[misc]
 async def get_config_amnezia_vpn(message: Message, state: FSMContext) -> None:
     """Пользователь получит конфиг файл amnezia_vpn."""
-    await message.answer(
-        "На свой конфиг amneziaVPN", reply_markup=ReplyKeyboardRemove()
-    )
-    await state.clear()
+    async with ChatActionSender.typing(bot=bot, chat_id=message.chat.id):
+        await message.answer(
+            "На свой конфиг amneziaVPN", reply_markup=ReplyKeyboardRemove()
+        )
+        await state.clear()
 
 
 @vpn_router.message(F.text.contains("🌐 Получить VPN-конфиг AmneziaWG"))  # type: ignore[misc]
 async def get_config_amnezia_wg(message: Message, state: FSMContext) -> None:
     """Пользвоатель получит конфиг Amnezia WG."""
-    await message.answer("На свой конфиг amneziaWG", reply_markup=ReplyKeyboardRemove())
-    await state.clear()
+    async with ChatActionSender.typing(bot=bot, chat_id=message.chat.id):
+        await message.answer(
+            "На свой конфиг amneziaWG", reply_markup=ReplyKeyboardRemove()
+        )
+        await state.clear()
 
 
 @vpn_router.message(F.text.contains("📈 Проверить статус подписки"))  # type: ignore[misc]
@@ -38,11 +43,14 @@ async def check_subscription(
     message: Message, session: AsyncSession, state: FSMContext
 ) -> None:
     """Проверка статуса подписки пользователем."""
-    s_user = SUserTelegramID(telegram_id=message.from_user.id)
-    user = await UserDAO.find_one_or_none(session=session, filters=s_user)
-    await message.answer(
-        "Проверка статуса подписки", reply_markup=ReplyKeyboardRemove()
-    )
-    print(user.subscription)
-    await bot.send_message(chat_id=message.from_user.id, text=str(user.subscription))
-    await state.clear()
+    async with ChatActionSender.typing(bot=bot, chat_id=message.chat.id):
+        s_user = SUserTelegramID(telegram_id=message.from_user.id)
+        user = await UserDAO.find_one_or_none(session=session, filters=s_user)
+        await message.answer(
+            "Проверка статуса подписки", reply_markup=ReplyKeyboardRemove()
+        )
+        print(user.subscription)
+        await bot.send_message(
+            chat_id=message.from_user.id, text=str(user.subscription)
+        )
+        await state.clear()
