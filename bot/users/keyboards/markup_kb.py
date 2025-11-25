2edffa57fa87daaca03c9b1f0b1fafc6932c4355
BoriskaGlebov <boristheblade.glebov@yandex.ru>
2025-11-25 23:00:24 +0300

    поменьше сделал PR

--- bot/users/keyboards/markup_kb.py
+++ bot/users/keyboards/markup_kb.py
@@ -1,43 +1,8 @@
-from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
-
-from bot.config import settings_bot
+from aiogram.types import ReplyKeyboardMarkup
 
 
 def main_kb(
     active_subscription: bool = False,
     user_telegram_id: int | None = None,
 ) -> ReplyKeyboardMarkup:
-    """Формирует клавиатуру главного меню бота.
-
-    Args:
-        active_subscription (bool): Подписка активна или нет
-        user_telegram_id (Optional[int]): Telegram ID пользователя, который вызывает клавиатуру.
-            Если None, отображаются только обычные пользовательские кнопки.
-
-    Returns
-        ReplyKeyboardMarkup: Клавиатура для пользователя.
-
-    Пример использования:
-        active_subscription=True
-        user_telegram_id = 123456789
-        keyboard = main_kb(user_telegram_id)
-        await message.answer("Выберите действие:", reply_markup=keyboard)
-
-    """
-    kb_list = []
-    if active_subscription:
-        kb_list.append([KeyboardButton(text="🔑 Получить VPN-конфиг AmneziaVPN")])
-        kb_list.append([KeyboardButton(text="🌐 Получить VPN-конфиг AmneziaWG")])
-        kb_list.append([KeyboardButton(text="💎 Продлить VPN-Boriska")])
-    else:
-        kb_list.append([KeyboardButton(text="💰 Выбрать подписку VPN-Boriska")])
-    kb_list.append([KeyboardButton(text="📈 Проверить статус подписки")])
-    kb_list.append([KeyboardButton(text="❓ Помощь в настройке VPN")])
-    if user_telegram_id in settings_bot.ADMIN_IDS:
-        kb_list.append([KeyboardButton(text="⚙️ Панель администратора")])
-
-    keyboard = ReplyKeyboardMarkup(
-        keyboard=kb_list, resize_keyboard=True, one_time_keyboard=True
-    )
-
-    return keyboard
+    pass
