34f347a7a6f569fff8786d4fc9d4db21300b00f4
BoriskaGlebov <boristheblade.glebov@yandex.ru>
2025-12-28 11:35:48 +0300

    Add ACME challenge location and nginx reload TODO for production

--- bot/main.py
+++ bot/main.py
@@ -88,6 +88,7 @@ async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
     await init_default_roles_admins()  # type: ignore
     await start_bot(bot=bot)
     # TODO Надо не забыть проверку подписок делать раз в день и не плохо мне об этом писать
+    # ToDO НА проде надо преезагружа ть nginx при обнолвние бота CI
     # TODO НАДО прикрутить реферальную программу
     scheduler.add_job(
         scheduled_check,
