b2ed40c1ab0c846aa73e4fea3eadeaf08a45136d
BoriskaGlebov <boristheblade.glebov@yandex.ru>
2026-03-14 23:15:13 +0300

    у пользователя возникли проблемы ошибка за удаление сообщения ошибка связана с тем что сообщение давно уже не валидно

--- bot/subscription/router.py
+++ bot/subscription/router.py
@@ -338,8 +338,10 @@ class SubscriptionRouter(BaseRouter):
         """
         msg = query.message
         if not msg:
+            self.logger.warning("Сообщения нет, оно уже удалено")
             return
         if isinstance(msg, InaccessibleMessage):
+            self.logger.warning("Сообщение уже старое его нельзя удалить")
             return
         user_logger = self.logger.bind(
             user=query.from_user.username or query.from_user.id
