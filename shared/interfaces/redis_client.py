4aaa3cdc24f74fb1eaed3a158f3dc909e57088b2
BoriskaGlebov <boristheblade.glebov@yandex.ru>
2026-03-18 16:50:19 +0300

    база пока остается в боте и дублируется в api

---
+++ shared/interfaces/redis_client.py
@@ -0,0 +1,37 @@
+from abc import ABC, abstractmethod
+from typing import Any
+
+
+class RedisClientProtocol(ABC):
+    """Абстракция клиента хранилища (Redis-like)."""
+
+    @abstractmethod
+    async def get(self, key: str) -> Any:
+        """Метод получения значения по ключу."""
+        ...
+
+    @abstractmethod
+    async def set(
+        self,
+        key: str,
+        value: Any,
+        expire: int | None = None,
+        nx: bool = False,
+    ) -> bool | None:
+        """Метод установки значения."""
+        ...
+
+    @abstractmethod
+    async def delete(self, key: str) -> None:
+        """Удалить значения."""
+        ...
+
+    @abstractmethod
+    async def connect(self) -> None:
+        """Подключения в Redis."""
+        ...
+
+    @abstractmethod
+    async def disconnect(self) -> None:
+        """Отключения от базы."""
+        ...
