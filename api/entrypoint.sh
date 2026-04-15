540fa7021934a8bcd6bddc126b59690ba231ce24
BoriskaGlebov <boristheblade.glebov@yandex.ru>
2026-04-15 22:08:34 +0300

    fix errors

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -8,16 +8,16 @@ chown -R botuser:bot /vpn_bot/api/logs
 echo "⚙️ Checking for Alembic migrations..."
 
 # Проверяем наличие alembic-конфигурации
-#if [ -d "api/migrations" ] && [ -f "alembic.ini" ]; then
-#    echo "📦 Alembic detected — applying migrations..."
-#    alembic upgrade head
-
-#    echo "🧹 Cleaning up Alembic migration files..."
-#    rm -rf api/migrations alembic.ini || true
-#    echo "✅ Migrations applied and Alembic removed."
-#else
-#    echo "ℹ️ Alembic not found — skipping migrations."
-#fi
+if [ -d "api/migrations" ] && [ -f "alembic.ini" ]; then
+    echo "📦 Alembic detected — applying migrations..."
+    alembic upgrade head
+#
+    echo "🧹 Cleaning up Alembic migration files..."
+    rm -rf api/migrations alembic.ini || true
+    echo "✅ Migrations applied and Alembic removed."
+else
+    echo "ℹ️ Alembic not found — skipping migrations."
+fi
 
 echo "🚀 Starting api..."
 exec su -s /bin/bash botuser -c  "python -m api.main"
